f6e0482437836df42e267daa6d786e55f3851086
Tanmay Patil <tanmay.gp91@gmail.com>
2018-02-02 15:18:12 +0530

    feat: setup react-md with sass

--- cric-sim/src/App.js
+++ cric-sim/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import { NavigationDrawer } from 'react-md';
 import logo from './logo.svg';
-import './App.css';
+import './styles/app.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <NavigationDrawer
+        drawerTitle="react-md with CRA"
+        toolbarTitle="Welcome to react-md">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+      </NavigationDrawer>
     );
   }
 }
--- cric-sim/src/index.js
+++ cric-sim/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './styles/index.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFontLoader from 'webfontloader';
+
+WebFontLoader.load({
+  google: {
+    families: ['Material Icons', 'Open Sans:400, 700', 'Montserrat:400, 700'],
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
